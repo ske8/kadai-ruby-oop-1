e03f6bd4bdd402f1b3b02fd6d1f8a4a0c5b89a8b
keisuke.okamura <cointoss69@icloud.com>
2023-07-30 08:30:39 +0000

    oop

---
+++ team.rb
@@ -0,0 +1,36 @@
+class Team
+  # オブジェクトの変数（値）
+  attr_accessor :name,:win,:lose,:draw
+
+  # 初期化用の特別なメソッド
+  def initialize(name,win,lose,draw)
+    self.name = name
+    self.win = win
+    self.lose = lose
+    self.draw = draw
+  end
+  
+  def calc_win_rate
+    return self.win.to_f/(self.win+self.lose)
+  end
+
+  # オブジェクトのメソッド（処理）
+  def show_team_result
+    puts "#{self.name}の2022年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{self.calc_win_rate}です。"
+    
+  end
+end
+
+giants = Team.new("Giants",67,45,8)
+tigers = Team.new("Tigers",60,53,7)
+dragons = Team.new("Dragons",60,55,5)
+bayStars = Team.new("BayStars",56,58,6)
+carp = Team.new("Carp",52,56,12)
+swallows = Team.new("Swallows",41,69,10)
+
+giants.show_team_result
+tigers.show_team_result
+dragons.show_team_result
+bayStars.show_team_result
+carp.show_team_result
+swallows.show_team_result
